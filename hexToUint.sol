8affa8bb846ff9d558c5480d060a420502b44739
Anthony <63381943+4nth0nySLT@users.noreply.github.com>
2022-01-01 18:54:56 -0500

    Update hexToUint.sol

--- hexToUint.sol
+++ hexToUint.sol
@@ -5,13 +5,12 @@ contract Test {
 
 // Implementation of eth_utils.decode_hex (python) in solidity. Only Numbers.
 
-function hexToUint(bytes memory data) private pure returns(uint number){
-    number=0;
+    function hexToUint(bytes memory data) private pure returns(uint number){
     
-    for (uint i = 0; i < data.length; i++) 
-      {
-        number += (uint256(uint8(data[i])) & 15 ) * 10 **(data.length-i-1);
-      }
-      
-   }
+        for (uint i = 0; i < data.length; i++) 
+          {
+            number += (uint256(uint8(data[i])) & 15 ) * 10 **(data.length-i-1);
+          }
+
+     }
 }
