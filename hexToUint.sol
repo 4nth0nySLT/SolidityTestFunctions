4fdae56eb58b6996d2a4fd07f30eb7a71bb40886
Anthony <63381943+4nth0nySLT@users.noreply.github.com>
2022-01-01 18:45:50 -0500

    Create hexToUint.sol

---
+++ hexToUint.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+contract Test {
+
+// Implementation of eth_utils.decode_hex (python) in solidity. Only Numbers.
+
+function hexToUint(bytes memory data) private pure returns(uint number){
+    number=0;
+    
+    for (uint i = 0; i < data.length; i++) 
+      {
+        number += (uint256(uint8(data[i])) & 15 ) * 10 **(data.length-i-1);
+      }
+      
+   }
+}
